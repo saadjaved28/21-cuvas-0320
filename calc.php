e0aa430a474a0159610ca6b7a1c8053ccd8a96c1
saadjaved28 <146163571+saadjaved28@users.noreply.github.com>
2023-09-27 09:55:23 -0700

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<h1> calculater </h1>";
+
+
+$a = "10";
+$b = "56";
+    
+    $sum = $a + $b;
+    echo" $sum<h2>add</h2>";
+
+    $sub = $a - $b;
+    echo"$sub<h2>sub</h2>";
+
+    $multi = $a * $b;
+    echo"$multi<h2>Multiple</h2>";
+
+    $Divide = $a / $b;
+    echo"$Divide<h2>Divide</h2>";
+
+
+ 
+
+?>
